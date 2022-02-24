ff96070350cc9dd719b642f8210119ce45dfecd5
Terry Babyuk <tbabyuk@gmail.com>
2022-02-23 23:59:57 -0500

    Express Week 1 Workshop - Task 1 - completed

--- routes/campsiteRouter.js
+++ routes/campsiteRouter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const campsiteRouter = express.Router();
 
+
+//Routing to home page
 campsiteRouter.route("/")
 
 .all((req, res, next) => {
@@ -23,4 +25,32 @@ campsiteRouter.route("/")
 });
 
 
+//Week 1 Workshop, Task 1
+//Routing to a specific campsite
+campsiteRouter.route("/:campsiteId")
+
+.all((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    next();
+})
+.get((req, res) => {
+    res.end(`Will send details of the campsite: ${req.params.campsiteId} to you`);
+})
+.post((req, res) => {
+    res.statusCode = 403;
+    res.end(`POST operation not supported on /campsites/${req.params.campsiteId}`)
+})
+.put((req, res) => {
+    res.write(`Updating the campsite: ${req.params.campsiteId}\n`);
+    res.end(`Will update the campsite: ${req.body.name}
+        with description: ${req.body.description}`);
+})
+.delete((req, res) => {
+    res.end(`Deleting campsite: ${req.params.campsiteId}`);
+});
+
+
 module.exports = campsiteRouter;
+
+
--- server.js
+++ server.js
@@ -31,13 +31,13 @@ app.use("/campsites", campsiteRouter);
 //     res.end(`Deleting campsite: ${req.params.campsiteId}`);
 // })
 
-app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/public"));
 
-app.use((req, res) => {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html");
-    res.end("<html><body><h1>This is an Express Server</h1></body></html>")
-});
+// app.use((req, res) => {
+//     res.statusCode = 200;
+//     res.setHeader("Content-Type", "text/html");
+//     res.end("<html><body><h1>This is an Express Server</h1></body></html>")
+// });
 
 app.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}: ${port}`)
